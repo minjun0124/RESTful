6a06520a43ba5404c83c62ab66534dc5ec9b4d62
mjlee <minjun0124@gmail.com>
2021-08-01 00:55:51 +0900

    feat: swagger ui costom

--- src/main/java/tutorial/restful/user/User.java
+++ src/main/java/tutorial/restful/user/User.java
@@ -3,6 +3,8 @@ package tutorial.restful.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,16 +18,22 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 //@JsonIgnoreProperties(value = {"password", "ssn"})
 //@JsonFilter("UserInfo")
+@ApiModel(description = "User Detail Info Domain instance")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Min length of name parameter is 2")
+    @ApiModelProperty(notes = "Enter username")
     private String name;
     @Past
+    @ApiModelProperty(notes = "Enter join date")
     private LocalDateTime joinDate;
 
 //    @JsonIgnore
+    @ApiModelProperty(notes = "Enter password")
     private String password;
 //    @JsonIgnore
+    @ApiModelProperty(notes = "Enter ssn")
     private String ssn;
+
 }
